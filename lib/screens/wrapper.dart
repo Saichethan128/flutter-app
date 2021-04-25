609ac1fb6dfa53cd4a3ed6970408b552af1ac4b2
SaiChethan <d.saichethan@gmail.com>
2021-04-25 22:35:04 +0530

    flutter app

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app/screens/home/MainPage.dart';
+class Wrapper extends StatefulWidget {
+  @override
+  _WrapperState createState() => _WrapperState();
+}
+
+class _WrapperState extends State<Wrapper> {
+  @override
+  Widget build(BuildContext context) {
+    return MainPage();
+  }
+}
